67c78fa1354d02b68e3b2c063b82477d0d102d8f
BOOLRon <ron.zhang.36@gmail.com>
2016-07-07 15:24:16 +0800

    convert to es5

--- DoubanMovieReviewReader.js
+++ DoubanMovieReviewReader.js
@@ -1,19 +1,15 @@
-/**
- * Created by Ron on 25/6/2016.
- */
-"use strict";
 
 function fetchImageContentFromHtmlLink(link,content,callback){
 
-  _fetchSourceBy(link,(source,error)=>{
+  _fetchSourceBy(link,function(source,error){
     if(error){
-     callback(null,error)
+     callback(null,error);
     }else {
-      let imagelinks = _pickImageUrlsFromSource(source)
-      let imageContent = _createImageContent(content,imagelinks)
-      callback(imageContent,null)
+      var imagelinks = _pickImageUrlsFromSource(source);
+      var imageContent = _createImageContent(content,imagelinks);
+      callback(imageContent,null);
     }
-  })
+  });
 
   return _createImageContent(content,undefined);
 
@@ -21,65 +17,59 @@ function fetchImageContentFromHtmlLink(link,content,callback){
 
 function _createImageContent(content,links){
 
-  let regex = /\[img=\d+:C\].*?\[\/img\]/g;
-  let originImageTag = content.match(regex)
+  var regex = /\[img=\d+:C\].*?\[\/img\]/g;
+  var originImageTag = content.match(regex);
   if (originImageTag === null){
-    originImageTag = []
+    originImageTag = [];
   }
   var resultContent = content;
 
-  //console.log(originImageTag.length)
-  //console.log(links.length)
-  //console.log(resultContent)
-
   if (links && originImageTag.length == links.length ) {
     originImageTag.forEach(function (tag, index, array) {
-      let imageLink = links[index]
+      var imageLink = links[index];
       if (imageLink) {
-        let newTag = tag.replace(/\].*?\[\/img\]/, (replacement)=> {
+        var newTag = tag.replace(/\].*?\[\/img\]/, function(replacement) {
           return '\<br\>\<div class=\"cc\"\>\<img src=\"' + imageLink +
             '\"\<div style=\"text-align:center;padding-bottom:1em\" class=\"wr pl\"\>\<div class=\"pl\" style=\"text-align:center;\"\>' +
-            replacement.substr(1, replacement.length - 7) + '\<\/div\>\<\/div\>\<\/div\>\<br\>'
-        })
-        let resultLink = newTag.replace(/\[img=\d+:C/, '')
-        resultContent = resultContent.replace(tag, resultLink)
+            replacement.substr(1, replacement.length - 7) + '\<\/div\>\<\/div\>\<\/div\>\<br\>';
+        });
+        var resultLink = newTag.replace(/\[img=\d+:C/, '');
+        resultContent = resultContent.replace(tag, resultLink);
       }
-    })
+    });
   }else {
     //pick error, replace all ImageTags
     originImageTag.forEach(function (tag, index, array) {
-      resultContent = resultContent.replace(tag, '')
+      resultContent = resultContent.replace(tag, '');
     });
   }
   return resultContent;
 }
 
 function _pickImageUrlsFromSource(source){
-  let regex = /<img src=\"https:\/\/img(.)(.doubanio.com\/view\/thing_review)(.*?\")/g;
-  let matchWords = source.match(regex)
+  var regex = /<img src=\"https:\/\/img(.)(.doubanio.com\/view\/thing_review)(.*?\")/g;
+  var matchWords = source.match(regex);
   if (matchWords === null){
-    matchWords = []
+    matchWords = [];
   }
-  let links = matchWords.map((word)=> {
-    return word.substr(10, word.length - 11)
-  })
-  return links
+  var links = matchWords.map(function(word){
+    return word.substr(10, word.length - 11);
+  });
+  return links;
 }
 
 function _fetchSourceBy(link,callback) {
   if (link){
-    fetch(link).then((response)=>response.text())
-      .then((responseText)=>{
-        callback(responseText,null)
+    fetch(link).then(function(response){response.text();})
+      .then(function(responseText){
+        callback(responseText,null);
       })
-      .catch((error)=>{
-        callback(null,error)
+      .catch(function(error){
+        callback(null,error);
       });
   }else {
-    callback(null,null)
+    callback(null,null);
   }
 }
 
-module.exports = {
-  fetchImageContentFromHtmlLink
-}
+module.exports = fetchImageContentFromHtmlLink;
