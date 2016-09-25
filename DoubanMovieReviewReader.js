1136841093fa67476af5f7bafb8f2310f27566f4
BOOLRon <ron.zhang.36@gmail.com>
2016-09-25 15:44:38 +0800

    support new format of phototag

--- DoubanMovieReviewReader.js
+++ DoubanMovieReviewReader.js
@@ -10,32 +10,56 @@ function createImageContentFromHtmlSourceCode(source,content){
 
 function _createImageContent(content,links){
 
-  var regex = /\[img=\d+:C\].*?\[\/img\]/g;
+  var regex = /<图片\d+>/g;
   var originImageTag = content.match(regex);
-  if (originImageTag === null){
-    originImageTag = [];
-  }
   var resultContent = content;
-
-  if (links && originImageTag.length == links.length ) {
-    originImageTag.forEach(function (tag, index, array) {
-      var imageLink = links[index];
-      if (imageLink) {
-        var newTag = tag.replace(/\].*?\[\/img\]/, function(replacement) {
-          return '\<br\>\<div class=\"cc\"\>\<img src=\"' + imageLink +
-            '\"\<div style=\"text-align:center;padding-bottom:1em\" class=\"wr pl\"\>\<div class=\"pl\" style=\"text-align:center;\"\>' +
-            replacement.substr(1, replacement.length - 7) + '\<\/div\>\<\/div\>\<\/div\>\<br\>';
-        });
-        var resultLink = newTag.replace(/\[img=\d+:C/, '');
-        resultContent = resultContent.replace(tag, resultLink);
+  if (originImageTag === null){
+      regex = /\[img=\d+:C\].*?\[\/img\]/g;
+      originImageTag = content.match(regex);
+      if (originImageTag === null){
+         originImageTag = [];
       }
-    });
+      if (links && originImageTag.length == links.length ) {
+          originImageTag.forEach(function (tag, index, array) {
+              var imageLink = links[index];
+              if (imageLink) {
+                  var newTag = tag.replace(/\].*?\[\/img\]/, function(replacement) {
+                      return '\<br\>\<div class=\"cc\"\>\<img src=\"' + imageLink +
+                          '\"\<div style=\"text-align:center;padding-bottom:1em\" class=\"wr pl\"\>\<div class=\"pl\" style=\"text-align:center;\"\>' +
+                          replacement.substr(1, replacement.length - 7) + '\<\/div\>\<\/div\>\<\/div\>\<br\>';
+                  });
+                  var resultLink = newTag.replace(/\[img=\d+:C/, '');
+                  resultContent = resultContent.replace(tag, resultLink);
+              }
+          });
+      }else {
+          //pick error, replace all ImageTags
+          originImageTag.forEach(function (tag, index, array) {
+              resultContent = resultContent.replace(tag, '');
+          });
+      }
   }else {
-    //pick error, replace all ImageTags
-    originImageTag.forEach(function (tag, index, array) {
-      resultContent = resultContent.replace(tag, '');
-    });
+
+      //  /<图片\d+>/g;
+      if (links && originImageTag.length == links.length ) {
+          originImageTag.forEach(function (tag, index, array) {
+              var imageLink = links[index];
+              if (imageLink) {
+                  var resultLink = tag.replace(/<图片\d+>/, function(replacement) {
+                      return '\<br\>\<div class=\"cc\"\>\<img src=\"' + imageLink +
+                          '\"\<div style=\"text-align:center;padding-bottom:1em\" class=\"wr pl\"\>\<div class=\"pl\" style=\"text-align:center;\"\>' + '\<\/div\>\<\/div\>\<\/div\>\<br\>';
+                  });
+                  resultContent = resultContent.replace(tag, resultLink);
+              }
+          });
+      }else {
+          //pick error, replace all ImageTags
+          originImageTag.forEach(function (tag, index, array) {
+              resultContent = resultContent.replace(tag, '');
+          });
+      }
   }
+
   return resultContent;
 }
 
